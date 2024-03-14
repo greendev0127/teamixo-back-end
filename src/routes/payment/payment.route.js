b6fab3637eb059e398deeeee95d8779e691b9646
greendev0127 <daniellee920127@gmail.com>
2024-03-14 16:10:17 +0000

    update env key name

--- src/routes/payment/payment.route.js
+++ src/routes/payment/payment.route.js
@@ -1,7 +1,7 @@
 import AWS from "aws-sdk";
 import { Router } from "express";
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_S_ID);
 
 const router = Router();
 
